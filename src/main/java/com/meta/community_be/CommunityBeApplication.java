d18d8b498ba6587dce41718251c5a8021fde036c
seoyeonpark906 <lilycole1124@gmail.com>
2026-01-27 11:06:44 +0900

    chore: First init

---
+++ src/main/java/com/meta/community_be/CommunityBeApplication.java
@@ -0,0 +1,13 @@
+package com.meta.community_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunityBeApplication.class, args);
+    }
+
+}
